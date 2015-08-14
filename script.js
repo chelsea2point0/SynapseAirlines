0153ad317a2aea28425a45f1d277d0caa64fb03c
chelsea2point0 <chelseajoyschaefer@gmail.com>
2015-08-14 13:18:49 -0400

    added css to make site look like airline homepage. added html content for menu, logo, and cover image

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 // Class to represent a row in the seat reservations grid
-function SeatReservation(name, initialMeal) {
+function SeatReservation(name, initialMeal, initialLocation) {
     var self = this;
     self.name = name;
     self.meal = ko.observable(initialMeal);
+    self.location = ko.observable(initialLocation);
 
     self.formattedPrice = ko.computed(function() {
         var price = self.meal().price;
-        return price ? "$" + price.toFixed(2) : "None";        
+        return price ? "$" + price.toFixed(2) : "Complementary";        
     });    
 }
 
@@ -17,14 +18,21 @@ function ReservationsViewModel() {
     // Non-editable catalog data - would come from the server
     self.availableMeals = [
         { mealName: "Standard (sandwich)", price: 0 },
+        { mealName: "Vegetarian (salad)", price: 0 },
         { mealName: "Premium (lobster)", price: 34.95 },
         { mealName: "Ultimate (whole zebra)", price: 290 }
     ];    
 
+    self.availableLocations = [
+        { seatType: "Aisle"},
+        { seatType: "Middle"},
+        { seatType: "Window"}
+    ];
+
     // Editable data
     self.seats = ko.observableArray([
-        new SeatReservation("Steve", self.availableMeals[0]),
-        new SeatReservation("Bert", self.availableMeals[0])
+        new SeatReservation("Jane Doe", self.availableMeals[2], self.availableLocations[2]),
+        new SeatReservation("John Doe", self.availableMeals[3], self.availableLocations[0])
     ]);
 
     // Computed data
@@ -37,7 +45,7 @@ function ReservationsViewModel() {
 
     // Operations
     self.addSeat = function() {
-        self.seats.push(new SeatReservation("", self.availableMeals[0]));
+        self.seats.push(new SeatReservation("", self.availableMeals[0], self.availableLocations[0]));
     }
     self.removeSeat = function(seat) { self.seats.remove(seat) }
 }
